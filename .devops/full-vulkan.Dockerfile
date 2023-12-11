f49de02d090013f4c96f7d6c37b8d942fa26cfd1
Jennifer Wilcox <jennifer.wilcox@adtran.com>
2023-12-11 15:47:42 -0600

    Fix up cmake build too

--- .devops/full-vulkan.Dockerfile
+++ .devops/full-vulkan.Dockerfile
@@ -5,5 +5,5 @@ FROM ubuntu:$UBUNTU_VERSION as build
 RUN apt-get update && \
     apt-get install -y build-essential python3 python3-pip git
 
-RUN apt-get install -y libvulkan-dev glslc
+RUN apt-get install -y libvulkan-dev glslc cmake
 ENV LC_ALL=C.utf8
--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -eux -o pipefail
 
 docker build . -f .devops/full-vulkan.Dockerfile -t llama-cpp-builder:local
 
+mkdir -p build
+
 docker run -it --rm \
     -v "$(pwd):$(pwd)" \
     -w "$(pwd)" \
     -u "$(id -u):$(id -g)" \
     llama-cpp-builder:local \
-    make -j 10 LLAMA_VULKAN=1
+    /bin/bash -c "cd build && cmake .. -DLLAMA_VULKAN=ON && make -j 10"
